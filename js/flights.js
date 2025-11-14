a1f00a863b33b7eaa792f6cf506cba153d254f26
Edward Dong <115418640+DaiLoDong@users.noreply.github.com>
2025-11-14 14:47:25 -0700

    remove flights api, google already handles this

--- js/flights.js
+++ js/flights.js
@@ -1,41 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btn = document.getElementById("bookFlightBtn");
-  if (!btn) return; // button must exist
+  if (!btn) return;
 
   // constants
   const DEST = "YYJ";
-  const DEPART_DATE = "2027-10-09";
-  const RETURN_DATE = "2027-10-11";
-  const API_KEY = "j3vcH1rMbrvHYwDMLIRCUg==uUD7392RpH05rkOA";
-
-
-async function getGeoByIP() {
-  const res = await fetch("https://ipapi.co/json/");
-  const data = await res.json();
-  return { lat: data.latitude, lon: data.longitude };
-}
-
-
-async function getNearestAirport(lat, lon) {
-    const res = await fetch(
-      `https://api.api-ninjas.com/v1/airports?lat=${lat}&lon=${lon}`,
-      { headers: { "X-Api-Key": API_KEY } }
-    );
-    const airports = await res.json();
-    return airports[0]?.iata;
-  }
+  const DEPART_DATE = "2026-10-09";
+  const RETURN_DATE = "2026-10-11";
 
 // button click
   btn.addEventListener("click", async () => {
     console.log("clicked button");
-
-    const { lat, lon } = await getGeoByIP();
-    const origin = await getNearestAirport(lat, lon);
-
-    const url = origin
-      ? `https://www.google.com/flights?hl=en#flt=${origin}.${DEST}.${DEPART_DATE}*${DEST}.${origin}.${RETURN_DATE}`
-      : `https://www.google.com/flights?hl=en#dest=${DEST}.${DEPART_DATE}`;
-
+      const url = `https://www.google.com/flights?hl=en#flt=.${DEST}.${DEPART_DATE}*${DEST}..${RETURN_DATE}`;
     window.open(url, "_blank");
   });
 });
