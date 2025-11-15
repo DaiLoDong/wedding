a620e69d3644c6925ad72d353a4649c652a934ee
Edward Dong <115418640+DaiLoDong@users.noreply.github.com>
2025-11-15 00:06:53 -0700

    fix button bug?

--- js/flights.js
+++ js/flights.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.getElementById("bookFlightBtn");
-  if (!btn) return;
+  const fbtn = document.getElementById("bookFlightBtn");
+  if (!fbtn) return;
 
   // constants
   const DEST = "YYJ";
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const RETURN_DATE = "2026-10-11";
 
 // button click
-  btn.addEventListener("click", async () => {
+  fbtn.addEventListener("click", async () => {
     console.log("clicked button");
       const url = `https://www.google.com/travel/flights?q=flights%20to%20${DEST}%20on%20${DEPART_DATE}%20return%20${RETURN_DATE}`;
     window.open(url, "_blank");
